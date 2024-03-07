b49e176c530adcd68b1a5d1bbbbbe13a4f989df8
omvaibhav9 <154995597+omvaibhav9@users.noreply.github.com>
2024-03-07 10:28:18 +0530

    Update info.py

--- info.py
+++ info.py
@@ -25,8 +25,8 @@ PICS = (environ.get('PICS', 'https://telegra.ph/file/6913041066ba687056237.jpg')
 NOR_IMG = environ.get("NOR_IMG", "https://graph.org/file/e20b5fdaf217252964202.jpg")
 MELCOW_VID = environ.get("MELCOW_VID", "https://telegra.ph/file/85d361ab4cb6511006022.mp4")
 SPELL_IMG = environ.get("SPELL_IMG", "https://telegra.ph/file/86b7b7e2aa7e38f328902.jpg")
-SUBSCRIPTION = (environ.get('SUBSCRIPTION', 'https://graph.org/file/c5d0cc345b0aa0275b059.jpg'))
-CODE = (environ.get('CODE', 'https://graph.org/file/c5d0cc345b0aa0275b059.jpg'))
+SUBSCRIPTION = (environ.get('SUBSCRIPTION', 'https://graph.org/file/5b4531bb88bad855e4680.jpg'))
+CODE = (environ.get('CODE', 'https://graph.org/file/5b4531bb88bad855e4680.jpg'))
 
 #stream link shortner
 STREAM_SITE = (environ.get('STREAM_SITE', 'ziplinker.net'))
